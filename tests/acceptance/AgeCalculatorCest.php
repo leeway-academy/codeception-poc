4ec74628ea997dae065dd2a76187d2f190d9eaf9
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2020-07-18 15:49:38 -0300

    Added waiting to improve presentation

--- tests/acceptance/AgeCalculatorCest.php
+++ tests/acceptance/AgeCalculatorCest.php
@@ -18,10 +18,14 @@ class FirstCest
         $name = 'Mauro';
         $birthday = '22/12/1977';
         $I->fillField('firstName', $name);
+        $I->wait(1);
         $I->fillField('birthday', $birthday);
+        $I->wait(1);
         $I->pause();
         $I->click('Calculate age' );
+        $I->wait(1);
         $I->see('42', 'h2');
         $I->see('Hi '.$name, 'h1');
+        $I->wait(1);
     }
 }
